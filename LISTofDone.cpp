e540b69d6f93fe85d8cf5afa67146080da826879
Shubhi Dixit <shubhi.dixit.dtu@gmail.com>
2026-07-09 00:55:48 +0530

    Add files via upload

---
+++ LISTofDone.cpp
@@ -0,0 +1,218 @@
+/*
+Solved leetcode-
+1. Two Sum
+57.8%
+Easy
+
+2. Add Two Numbers
+48.8%
+Med.
+
+3. Longest Substring Without Repeating Characters
+39.4%
+Med.
+
+4. Median of Two Sorted Arrays
+47.0%
+Hard
+
+7. Reverse Integer
+32.1%
+Med.
+
+9. Palindrome Number
+60.7%
+Easy
+
+10. Regular Expression Matching
+31.2%
+Hard
+
+13. Roman to Integer
+66.8%
+Easy
+
+15. 3Sum
+39.4%
+Med.
+
+20. Valid Parentheses
+44.4%
+Easy
+
+21. Merge Two Sorted Lists
+68.5%
+Easy
+
+22. Generate Parentheses
+78.9%
+Med.
+
+24. Swap Nodes in Pairs
+69.8%
+Med.
+
+27. Remove Element
+62.1%
+Easy
+
+29. Divide Two Integers
+20.0%
+Med.
+
+41. First Missing Positive
+43.2%
+Hard
+
+62. Unique Paths
+67.0%
+Med.
+
+66. Plus One
+50.2%
+Easy
+
+67. Add Binary
+58.2%
+Easy
+
+69. Sqrt(x)
+42.0%
+Easy
+
+70. Climbing Stairs
+54.2%
+Easy
+
+75. Sort Colors
+70.0%
+Med.
+
+88. Merge Sorted Array
+55.2%
+Easy
+
+94. Binary Tree Inorder Traversal
+80.3%
+Easy
+
+102. Binary Tree Level Order Traversal
+73.0%
+Med.
+
+121. Best Time to Buy and Sell Stock
+57.0%
+Easy
+
+136. Single Number
+77.9%
+Easy
+
+144. Binary Tree Preorder Traversal
+76.1%
+Easy
+
+145. Binary Tree Postorder Traversal
+78.5%
+Easy
+
+169. Majority Element
+66.4%
+Easy
+
+202. Happy Number
+59.9%
+Easy
+
+203. Remove Linked List Elements
+54.8%
+Easy
+
+204. Count Primes
+36.2%
+Med.
+
+206. Reverse Linked List
+80.8%
+Easy
+
+209. Minimum Size Subarray Sum
+52.1%
+Med.
+
+223. Rectangle Area
+49.8%
+Med.
+
+234. Palindrome Linked List
+58.3%
+Easy
+
+258. Add Digits
+69.1%
+Easy
+
+263. Ugly Number
+43.8%
+Easy
+
+278. First Bad Version
+47.3%
+Easy
+
+279. Perfect Squares
+56.7%
+Med.
+
+287. Find the Duplicate Number
+64.6%
+Med.
+
+326. Power of Three
+51.3%
+Easy
+
+349. Intersection of Two Arrays
+78.0%
+Easy
+
+387. First Unique Character in a String
+65.8%
+Easy
+
+412. Fizz Buzz
+75.7%
+Easy
+
+560. Subarray Sum Equals K
+47.7%
+Med.
+
+812. Largest Triangle Area
+71.7%
+Easy
+
+836. Rectangle Overlap
+46.8%
+Easy
+
+883. Projection Area of 3D Shapes
+76.3%
+Easy
+
+892. Surface Area of 3D Shapes
+71.1%
+Easy
+
+1143. Longest Common Subsequence
+59.3%
+Med.
+
+2965. Find Missing and Repeated Values
+83.1%
+Easy
+
+3483. Unique 3-Digit Even Numbers
+70.1%
+Easy
+*/
