4b6104d08997b3498cf60149de080dac16c00b29
Shubhi Dixit <shubhi.dixit.dtu@gmail.com>
2026-07-26 10:57:04 +0530

    Update LISTofDone.cpp

--- LISTofDone.cpp
+++ LISTofDone.cpp
@@ -1,83 +1,105 @@
-/*
-Solved leetcode-
+628. Maximum Product of Three Numbers
+46.5%
+Easy
+
 1. Two Sum
-57.8%
+57.9%
 Easy
 
 2. Add Two Numbers
-48.8%
+49.0%
 Med.
 
 3. Longest Substring Without Repeating Characters
-39.4%
+39.6%
 Med.
 
 4. Median of Two Sorted Arrays
-47.0%
+47.1%
 Hard
 
 7. Reverse Integer
-32.1%
+32.2%
 Med.
 
 9. Palindrome Number
-60.7%
+60.8%
 Easy
 
 10. Regular Expression Matching
-31.2%
+31.4%
 Hard
 
 13. Roman to Integer
-66.8%
+66.9%
+Easy
+
+14. Longest Common Prefix
+48.0%
 Easy
 
 15. 3Sum
-39.4%
+39.5%
+Med.
+
+17. Letter Combinations of a Phone Number
+66.5%
 Med.
 
 20. Valid Parentheses
-44.4%
+44.6%
 Easy
 
 21. Merge Two Sorted Lists
-68.5%
+68.6%
 Easy
 
 22. Generate Parentheses
-78.9%
+79.0%
 Med.
 
 24. Swap Nodes in Pairs
-69.8%
+70.0%
 Med.
 
 27. Remove Element
-62.1%
+62.2%
+Easy
+
+28. Find the Index of the First Occurrence in a String
+47.0%
 Easy
 
 29. Divide Two Integers
-20.0%
+20.1%
 Med.
 
 41. First Missing Positive
-43.2%
+43.4%
+Hard
+
+42. Trapping Rain Water
+67.9%
+Hard
+
+44. Wildcard Matching
+32.4%
 Hard
 
 62. Unique Paths
-67.0%
+67.1%
 Med.
 
 66. Plus One
-50.2%
+50.3%
 Easy
 
 67. Add Binary
-58.2%
+58.4%
 Easy
 
 69. Sqrt(x)
-42.0%
+42.2%
 Easy
 
 70. Climbing Stairs
@@ -85,51 +107,115 @@ Easy
 Easy
 
 75. Sort Colors
-70.0%
+70.1%
 Med.
 
+76. Minimum Window Substring
+48.1%
+Hard
+
+77. Combinations
+74.9%
+Med.
+
+79. Word Search
+47.8%
+Med.
+
+83. Remove Duplicates from Sorted List
+57.2%
+Easy
+
+85. Maximal Rectangle
+59.4%
+Hard
+
 88. Merge Sorted Array
-55.2%
+55.3%
 Easy
 
 94. Binary Tree Inorder Traversal
-80.3%
+80.4%
+Easy
+
+98. Validate Binary Search Tree
+36.1%
+Med.
+
+100. Same Tree
+67.6%
+Easy
+
+101. Symmetric Tree
+61.7%
 Easy
 
 102. Binary Tree Level Order Traversal
-73.0%
+73.1%
 Med.
 
-121. Best Time to Buy and Sell Stock
-57.0%
+110. Balanced Binary Tree
+58.8%
 Easy
 
+121. Best Time to Buy and Sell Stock
+57.1%
+Easy
+
+129. Sum Root to Leaf Numbers
+70.2%
+Med.
+
+134. Gas Station
+48.4%
+Med.
+
 136. Single Number
-77.9%
+78.0%
+Easy
+
+139. Word Break
+49.7%
+Med.
+
+141. Linked List Cycle
+54.8%
 Easy
 
 144. Binary Tree Preorder Traversal
-76.1%
+76.3%
 Easy
 
 145. Binary Tree Postorder Traversal
-78.5%
+78.7%
+Easy
+
+160. Intersection of Two Linked Lists
+64.3%
 Easy
 
 169. Majority Element
 66.4%
 Easy
 
+188. Best Time to Buy and Sell Stock IV
+50.8%
+Hard
+
+198. House Robber
+53.4%
+Med.
+
 202. Happy Number
-59.9%
+60.0%
 Easy
 
 203. Remove Linked List Elements
-54.8%
+55.0%
 Easy
 
 204. Count Primes
-36.2%
+36.3%
 Med.
 
 206. Reverse Linked List
@@ -137,27 +223,51 @@ Med.
 Easy
 
 209. Minimum Size Subarray Sum
-52.1%
+52.3%
+Med.
+
+213. House Robber II
+45.3%
+Med.
+
+221. Maximal Square
+50.6%
 Med.
 
 223. Rectangle Area
-49.8%
+49.9%
 Med.
 
-234. Palindrome Linked List
-58.3%
+225. Implement Stack using Queues
+70.5%
 Easy
+
+232. Implement Queue using Stacks
+70.2%
+Easy
+
+234. Palindrome Linked List
+58.4%
+Easy
+
+238. Product of Array Except Self
+69.1%
+Med.
+
+241. Different Ways to Add Parentheses
+73.4%
+Med.
 
 258. Add Digits
 69.1%
 Easy
 
 263. Ugly Number
-43.8%
+43.9%
 Easy
 
 278. First Bad Version
-47.3%
+47.4%
 Easy
 
 279. Perfect Squares
@@ -165,54 +275,150 @@ Easy
 Med.
 
 287. Find the Duplicate Number
-64.6%
+64.7%
 Med.
 
 326. Power of Three
-51.3%
+51.4%
 Easy
 
 349. Intersection of Two Arrays
-78.0%
+78.1%
 Easy
 
+386. Lexicographical Numbers
+76.3%
+Med.
+
 387. First Unique Character in a String
-65.8%
+65.9%
 Easy
 
 412. Fizz Buzz
 75.7%
 Easy
 
+456. 132 Pattern
+35.0%
+Med.
+
+503. Next Greater Element II
+68.8%
+Med.
+
+543. Diameter of Binary Tree
+65.9%
+Easy
+
 560. Subarray Sum Equals K
-47.7%
+47.8%
+Med.
+
+628. Maximum Product of Three Numbers
+46.4%
+Easy
+
+647. Palindromic Substrings
+73.0%
+Med.
+
+713. Subarray Product Less Than K
+54.8%
 Med.
 
 812. Largest Triangle Area
-71.7%
+71.8%
 Easy
 
 836. Rectangle Overlap
-46.8%
+46.9%
+Easy
+
+876. Middle of the Linked List
+82.2%
 Easy
 
 883. Projection Area of 3D Shapes
-76.3%
+76.4%
 Easy
 
 892. Surface Area of 3D Shapes
 71.1%
 Easy
 
-1143. Longest Common Subsequence
-59.3%
+918. Maximum Sum Circular Subarray
+50.9%
 Med.
+
+951. Flip Equivalent Binary Trees
+69.5%
+Med.
+
+1081. Smallest Subsequence of Distinct Characters
+70.5%
+Med.
+
+1143. Longest Common Subsequence
+59.4%
+Med.
+
+1260. Shift 2D Grid
+74.4%
+Easy
+
+1344. Angle Between Hands of a Clock
+69.7%
+Med.
+
+1979. Find Greatest Common Divisor of Array
+83.5%
+Easy
 
 2965. Find Missing and Repeated Values
 83.1%
 Easy
 
+3312. Sorted GCD Pair Queries
+58.4%
+Hard
+
+3336. Find the Number of Subsequences With Equal GCD
+66.7%
+Hard
+
 3483. Unique 3-Digit Even Numbers
-70.1%
+70.2%
 Easy
-*/
+
+3499. Maximize Active Section with Trade I
+61.0%
+Med.
+
+3501. Maximize Active Section with Trade II
+65.2%
+Hard
+
+3513. Number of Unique XOR Triplets I
+57.5%
+Med.
+
+3514. Number of Unique XOR Triplets II
+52.9%
+Med.
+
+3536. Maximum Product of Two Digits
+75.6%
+Easy
+
+3620. Network Recovery Pathways
+50.8%
+Hard
+
+3658. GCD of Odd and Even Sums
+89.8%
+Easy
+
+3867. Sum of GCD of Formed Pairs
+75.0%
+Med.
+
